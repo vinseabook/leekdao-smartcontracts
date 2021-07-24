da2c1ab3055377199fa4aea3bc5b374e20406300
cryptoseabook <cryptoseabook@gmail.com>
2021-07-24 15:10:24 +1000

    add Airdrop.sol and LuckyDraw.sol

---
+++ contracts/leekDao/Airdrop.sol
@@ -0,0 +1,68 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Airdrop is Ownable, AccessControl, ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeERC20 for ERC20;
+
+    // Create a new role identifier for the airdrop role
+    bytes32 public constant AIRDROP_ROLE = keccak256("AIRDROP_ROLE");
+    bytes32 public constant AIRDROP_VIP_ROLE = keccak256("AIRDROP_VIP_ROLE");
+
+    mapping(address => bool) private airdropSuccess;
+
+    uint public airdropAmount;
+    uint public vipAirdropAmount;
+
+    event AirdropEvent(bytes32 indexed requestId, uint roundNumber, address[] winners);
+
+    ERC20 public token;
+
+    constructor(ERC20 token_)
+    {
+        token = token_;
+        airdropAmount = 10 * 10 ** (token.decimals());
+        vipAirdropAmount = 100 * 10 ** (token.decimals());
+    }
+
+    function setAirdropAmount(uint amount_) public onlyOwner {
+        airdropAmount = amount_ * 10 ** (token.decimals());
+    }
+
+    function setVipAirdropAmount(uint amount_) public onlyOwner {
+        vipAirdropAmount = amount_ * 10 ** (token.decimals());
+    }
+
+    function whitelist(address[] memory addresses) public onlyOwner {
+        for(uint i = 0; i < addresses.length; i++) {
+          _setupRole(AIRDROP_ROLE, addresses[i]);
+        }
+    }
+
+    function whitelistVIP(address[] memory vipAddresses) public onlyOwner {
+        for(uint i = 0; i < vipAddresses.length; i++) {
+          _setupRole(AIRDROP_VIP_ROLE, vipAddresses[i]);
+        }
+    }
+
+    function getAirdrop() public nonReentrant {
+      require(hasRole(AIRDROP_ROLE, msg.sender) || hasRole(AIRDROP_VIP_ROLE, msg.sender), "You must be whitelisted or VIP!");
+      require(airdropSuccess[msg.sender] == false, "You have already collected airdrop!");
+
+      if (hasRole(AIRDROP_VIP_ROLE, msg.sender)) {
+        token.safeTransfer(msg.sender, airdropAmount);
+      } else {
+        token.safeTransfer(msg.sender, vipAirdropAmount);
+      }
+
+      airdropSuccess[msg.sender] = true;
+    }
+
+}
--- contracts/leekDao/LuckyDraw.sol
+++ contracts/leekDao/LuckyDraw.sol
@@ -2,75 +2,73 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract LuckyDraw is Ownable, VRFConsumerBase {
     using SafeMath for uint256;
-    using SafeERC20 for IERC20;
+    using SafeERC20 for ERC20;
 
     bytes32 internal keyHash;
     uint256 internal fee;
 
-    address[] public participants;
+    address[] private participants;
 
-    mapping(uint => address[]) public roundWinners;
-    mapping(uint => RoundInfo) public roundInfoMapping;
+    mapping(uint => address[]) private roundWinners;
+    mapping(uint => RoundInfo) private roundInfoMapping;
 
     uint public currentRound = 1;
-    uint public rewards = 10 * 10 ** 18;
+    uint public rewards;
 
-    event LuckyDrawRequest(bytes32 indexed requestId, uint roundNumber, uint drawNumber);
+    event LuckyDrawEvent(bytes32 indexed requestId, uint roundNumber, address[] winners);
 
-    // The token being sold
-    IERC20 public _token;
+    ERC20 public token;
 
     struct RoundInfo {
         bytes32 requestId;
         uint drawNumber;
     }
 
-    /**
-     * Constructor inherits VRFConsumerBase
-     *
-     * Network: Polygon (Matic) Mumbai Testnet
-     * Chainlink VRF Coordinator address: 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255
-     * LINK token address:                0x326C977E6efc84E512bB9C30f76E30c160eD06FB
-     * Key Hash: 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4
-     */
-    constructor(IERC20 token_)
+    constructor(ERC20 token_, address vrfCoordinator_, address linkToken_, bytes32 keyHash_, uint fees_)
         VRFConsumerBase(
-            0x8C7382F9D8f56b33781fE506E897a4F1e2d17255, // VRF Coordinator
-            0x326C977E6efc84E512bB9C30f76E30c160eD06FB  // LINK Token
+            vrfCoordinator_,
+            linkToken_
         )
     {
-        keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
-        fee = 0.0001 * 10 ** 18; // 0.0001 LINK (Varies by network)
-        _token = token_;
+        keyHash = keyHash_;
+        fee = fees_;
+        token = token_;
+        rewards = 10 * 10 ** (token.decimals());
     }
 
-    function participate(address[] memory addresses) onlyOwner public {
+    function setRewards(uint amount_) public onlyOwner {
+        rewards = amount_ * 10 ** (token.decimals());
+    }
+
+    function participate(address[] memory addresses) public onlyOwner {
         for(uint i = 0; i < addresses.length; i++) {
             participants.push(addresses[i]);
         }
     }
 
-    function luckyDraw(uint number) onlyOwner public {
+    function luckyDraw(uint number) public onlyOwner {
         bytes32 _requestId = getRandomNumber();
         roundInfoMapping[currentRound] = RoundInfo({
             requestId: _requestId,
             drawNumber: number
         });
+    }
 
-        emit LuckyDrawRequest(_requestId, currentRound, number);
+    function resetParticipants() public onlyOwner {
+        delete participants;
     }
 
     /**
      * Requests randomness
      */
-    function getRandomNumber() onlyOwner public returns (bytes32 requestId) {
+    function getRandomNumber() public onlyOwner returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
         return requestRandomness(keyHash, fee);
     }
@@ -81,9 +79,9 @@ contract LuckyDraw is Ownable, VRFConsumerBase {
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         RoundInfo storage roundInfo = roundInfoMapping[currentRound];
         bytes32 _requestId = roundInfo.requestId;
-        uint drawNo = roundInfo.drawNumber;
         require(_requestId == requestId, "The oracle requestId is not matched");
 
+        uint drawNo = roundInfo.drawNumber;
         address[] storage winners = roundWinners[currentRound];
 
         for (uint i = 0; i < drawNo; i++) {
@@ -94,10 +92,20 @@ contract LuckyDraw is Ownable, VRFConsumerBase {
             participants[randomIndex] = participants[participants.length - 1];
             participants.pop();
 
-            _token.safeTransfer(luckyPerson, rewards);
+            token.safeTransfer(luckyPerson, rewards);
             winners.push(luckyPerson);
         }
         currentRound++;
+        emit LuckyDrawEvent(requestId, currentRound, winners);
+    }
+
+    function getWinners(uint round) public view returns (address[] memory) {
+        require (round < currentRound, "The current round is not drawn yet!");
+        return roundWinners[round];
+    }
+
+    function getParticipants() public view returns (address[] memory) {
+        return participants;
     }
 
 }
